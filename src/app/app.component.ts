cb2b0dcd6f7cfce68c7a220ce4d960cb7201d38a
Mordechai Dror <vorant94@gmail.com>
2023-07-01 17:47:42 +0300

    basic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'amplify-functions-on-steroids';
+  private fb = inject(FormBuilder);
+
+  protected form = this.fb.group({
+    title: this.fb.control('', {nonNullable: true, validators: [Validators.required]})
+  })
+
+  protected createTodo(): void {
+    console.log('creating todo...');
+    this.form.reset();
+  }
+
+  protected setCompleted(value: boolean): void {
+    console.log(`setting completed to ${value}...`);
+  }
+
+  protected markAllAsComplete(): void {
+    console.log('marking all as complete...')
+  }
+
+  protected deleteAllCompleted(): void {
+    console.log('deleting all completed...')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
